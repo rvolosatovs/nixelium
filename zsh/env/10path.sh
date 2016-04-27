3b13b7406b3ce3aec2bab097910cb1e389f10da3
Romans Volosatovs <b1101@riseup.net>
2016-04-27 22:30:19 +0200

    setup go

--- zsh/env/10path.sh
+++ zsh/env/10path.sh
@@ -2,6 +2,7 @@ if [[ $distro == "exherbo" ]]; then
     path+=("/usr/local/bin/${CHOST}")
 fi
 
+path+=("/usr/local/heroku/bin")
 path=("${HOME}/.local/bin" $path)
 
 export PATH
---
+++ zsh/env/go.sh
@@ -0,0 +1,2 @@
+export GOPATH="$HOME/.local/go"
+path+=("$HOME/.local/go/bin")
