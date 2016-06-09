22ee2b7730022d6c4db84ff8c0823464162a7129
Romans Volosatovs <b1101@riseup.net>
2016-06-09 14:14:23 +0200

    09.06.16 upd

--- zsh/env/java.sh
+++ zsh/env/java.sh
@@ -1,1 +1,1 @@
-export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=full -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel' 
+export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel -Dswing.aatext=true -Dsun.java2d.xrender=true'
