395c0d88071a25fa7e2fdbce3b5d03c3663948ac
Roman Volosatovs <rvolosatovs@riseup.net>
2017-06-11 17:27:25 +0200

    nixos: update config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -42,6 +42,7 @@ fonts = {
     terminus_font
     dejavu_fonts
     font-awesome-ttf
+    siji
     fira
     fira-mono
   ];
@@ -71,7 +72,8 @@ programs = {
   zsh = {
     enable = true;
     enableAutosuggestions = true;
-    syntaxHighlighting.enable = true;
+    #syntaxHighlighting.enable = true;
+    enableSyntaxHighlighting = true;
     interactiveShellInit = ''
         source ${pkgs.grml-zsh-config}/etc/zsh/zshrc
         HISTFILE="''${ZDOTDIR:-$HOME}/.zhistory"
@@ -88,8 +90,8 @@ programs = {
   light.enable = true;
   java.enable = true;
   wireshark.enable = true;
-  slock.enable = true;
-  gnupg.agent.enable = true;
+  #slock.enable = true;
+  #gnupg.agent.enable = true;
 
   chromium = {
     enable = true;
@@ -98,11 +100,18 @@ programs = {
       "gcbommkclmclpchllfjekcdonpmejbdp" # https everywhere
       "klbibkeccnjlkjkiokjodocebajanakg" # great suspender
       "fjnbnpbmkenffdnngjfgmeleoegfcffe" # stylish
+      "ocifcklkibdehekfnmflempfgjhbedch" # ad block
     ];
   };
 };
 
 nixpkgs.config.allowUnfree = true;
+# related:
+# http://anderspapitto.com/posts/2015-11-01-nixos-with-local-nixpkgs-checkout.html
+# https://stackoverflow.com/questions/33294201/how-do-you-rebuild-nixos-packages-using-cloned-nixpkgs
+# http://matrix.ai/2017/03/13/intro-to-nix-channels-and-reproducible-nixos-environment
+nix.nixPath = [ "nixpkgs=/nix/nixpkgs" "nixos-config=/etc/nixos/configuration.nix" ];
+
 environment = {
   systemPackages = with pkgs; [
     # OS/Env
@@ -130,6 +139,7 @@ environment = {
     xsel
     #xorg.xset
     #xorg.xsetroot
+    xtitle
     xclip
     sxhkd
     slock
@@ -143,12 +153,14 @@ environment = {
     nodejs
     julia
     neovim
+    gcc
     gnumake
     gradle
     docker_compose
     docker-gc
     git
     git-lfs
+    universal-ctags
     gist
     fzf
     platinum-searcher
@@ -169,8 +181,10 @@ environment = {
     firefox
     chromium
     libreoffice
+    texlive.combined.scheme-full
     pandoc
     keybase
+    slock
     wget
     termite
     zathura
@@ -197,36 +211,43 @@ environment = {
     PAGER="less";
 
     QT_QPA_PLATFORMTHEME="gtk2";
+  };
 
-    XDG_CONFIG_HOME="$HOME/.config";
-    XDG_CACHE_HOME="$HOME/.local/cache";
-    XDG_DATA_HOME="$HOME/.local/share";
+  variables = {
+    XDG_CONFIG_HOME="\${HOME}/.config";
+    XDG_CACHE_HOME="\${HOME}/.local/cache";
+    XDG_DATA_HOME="\${HOME}/.local/share";
 
-    GIMP2_DIRECTORY="$XDG_CONFIG_HOME/gimp";
-    GNUPGHOME="$XDG_CONFIG_HOME/gnupg";
-    GTK2_RC_FILES="$XDG_CONFIG_HOME/gtk-2.0/gtkrc";
-    INPUTRC="$XDG_CONFIG_HOME/readline/inputrc";
-    ELINKS_CONFDIR="$XDG_CONFIG_HOME/elinks";
+    GIMP2_DIRECTORY="\${XDG_CONFIG_HOME}/gimp";
+    GNUPGHOME="\${XDG_CONFIG_HOME}/gnupg";
+    GTK2_RC_FILES="\${XDG_CONFIG_HOME}/gtk-2.0/gtkrc";
+    INPUTRC="\${XDG_CONFIG_HOME}/readline/inputrc";
+    ELINKS_CONFDIR="\${XDG_CONFIG_HOME}/elinks";
 
-    WINEPREFIX="$XDG_DATA_HOME/wine";
-    TERMINFO="$XDG_DATA_HOME/terminfo";
-    TERMINFO_DIRS=[ "$XDG_DATA_HOME/terminfo" "/usr/share/terminfo" ];
+    WINEPREFIX="\${XDG_DATA_HOME}/wine";
+    TERMINFO="\${XDG_DATA_HOME}/terminfo";
+    TERMINFO_DIRS=[ "\${XDG_DATA_HOME}/terminfo" "/usr/share/terminfo" ];
 
-    TMUX_TMPDIR="$XDG_RUNTIME_DIR/tmux";
+    TMUX_TMPDIR="\${XDG_RUNTIME_DIR}/tmux";
 
-    LESSHISTFILE="$XDG_CACHE_HOME/less/history";
-    __GL_SHADER_DISK_CACHE_PATH="$XDG_CACHE_HOME/nv";
-    CUDA_CACHE_PATH="$XDG_CACHE_HOME/nv";
-    PYTHON_EGG_CACHE="$XDG_CACHE_HOME/python-eggs";
+    LESSHISTFILE="\${XDG_CACHE_HOME}/less/history";
+    __GL_SHADER_DISK_CACHE_PATH="\${XDG_CACHE_HOME}/nv";
+    CUDA_CACHE_PATH="\${XDG_CACHE_HOME}/nv";
+    PYTHON_EGG_CACHE="\${XDG_CACHE_HOME}/python-eggs";
 
-    ZDOTDIR="$XDG_CONFIG_HOME/zsh";
-    ZPLUG_HOME="$ZDOTDIR/zplug";
+    ZDOTDIR="\${XDG_CONFIG_HOME}/zsh";
+    ZPLUG_HOME="\${ZDOTDIR}/zplug";
 
-    PASSWORD_STORE_DIR="$HOME/.local/pass";
+    PASSWORD_STORE_DIR="\${HOME}/.local/pass";
 
-    GOPATH="$HOME";
-    GOBIN="$HOME/.local/bin.go";
-    PATH=[ "$HOME/.local/bin" "$GOBIN" "$HOME/.gem/ruby/2.4.0/bin" ];
+    GOPATH="\${HOME}";
+    GOBIN="\${HOME}/.local/bin.go";
+    PATH=[ "\${HOME}/.local/bin" "\${GOBIN}" "\${HOME}/.gem/ruby/2.4.0/bin" ];
+
+    PANEL_FIFO="/tmp/panel-fifo";
+    PANEL_HEIGHT="24";
+    PANEL_FONT="-*-fixed-*-*-*-*-10-*-*-*-*-*-*-*";
+    PANEL_WM_NAME="bspwm_panel";
   };
 
   shells = [ 
@@ -266,10 +287,10 @@ services = {
 
     videoDrivers = [ "intel" ];
 
-    xautolock = {
-      enable = true;
-      locker = "${pkgs.slock}/bin/slock";
-    };
+    #    xautolock = {
+    #      enable = true;
+    #      locker = "${pkgs.slock}/bin/slock";
+    #    };
 
     desktopManager = {
       default = "none";
@@ -298,18 +319,20 @@ services = {
           # Java
           export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on -Dswing.defaultlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel -Dswing.aatext=true -Dsun.java2d.xrender=true'
 
+        #xrandr --output DP1 --primary --mode 3840x2160 --rate 60 --rotate normal --output eDP1 --auto --right-of DP1
+
           ${config.hardware.pulseaudio.package}/bin/pactl upload-sample /usr/share/sounds/freedesktop/stereo/bell.oga x11-bell
           ${config.hardware.pulseaudio.package}/bin/pactl load-module module-x11-bell sample=x11-bell display=$DISPLAY
 
           ${pkgs.feh}/bin/feh  --bg-max "$HOME/pictures/bg" 
-          ${pkgs.stalonetray}/bin/stalonetray -c "$XDG_CONFIG_HOME/stalonetray/stalonetrayrc" &
+          #${pkgs.stalonetray}/bin/stalonetray -c "''${XDG_CONFIG_HOME}/stalonetray/stalonetrayrc" &
           ${pkgs.dunst}/bin/dunst &
           ${pkgs.networkmanagerapplet}/bin/nm-applet &
           ${pkgs.xorg.xset}/bin/xset s off -dpms
           ${pkgs.xorg.xsetroot}/bin/xsetroot -cursor_name left_ptr
           ${pkgs.wmname}/bin/wmname LG3D
 
-          ${pkgs.sudo}/bin/sudo "$HOME/.local/bin/fix-keycodes"
+          ${pkgs.sudo}/bin/sudo "''${HOME}/.local/bin/fix-keycodes"
       '';
     };
     windowManager = {
@@ -342,6 +365,46 @@ services = {
         STOP_CHARGE_THRESH_BAT1=90
     '';
   };
+  udev.extraRules= ''
+      KERNEL="ttyUSB[0-9]*", TAG+="udev-acl", TAG+="uaccess", OWNER="rvolosatovs"
+      KERNEL="ttyACM[0-9]*", TAG+="udev-acl", TAG+="uaccess", OWNER="rvolosatovs"
+
+      SUBSYSTEM!="usb_device", ACTION!="add", GOTO="avrisp_end"
+
+      # Atmel Corp. JTAG ICE mkII
+      ATTR{idVendor}=="03eb", ATTRS{idProduct}=="2103", MODE="660", GROUP="dialout"
+      # Atmel Corp. AVRISP mkII
+      ATTR{idVendor}=="03eb", ATTRS{idProduct}=="2104", MODE="660", GROUP="dialout"
+      # Atmel Corp. Dragon
+      ATTR{idVendor}=="03eb", ATTRS{idProduct}=="2107", MODE="660", GROUP="dialout"
+
+      LABEL="avrisp_end"
+  '';
+  mopidy = {
+    enable = true;
+    configuration = ''
+      [local]
+      enabled = false
+      [soundcloud]
+      explore_songs = 25
+      [spotify]
+      bitrate = 320
+      timeout = 30
+      [spotify/tunigo]
+      enabled = true
+      [youtube]
+      enabled = true
+      '';
+    extensionPackages = [
+      pkgs.mopidy-moped
+      pkgs.mopidy-soundcloud
+      pkgs.mopidy-iris
+      pkgs.mopidy-mopify
+      pkgs.mopidy-spotify
+      pkgs.mopidy-spotify-tunigo
+      pkgs.mopidy-youtube
+    ];
+  };
 };
 virtualisation.docker.enable = true;
 
@@ -353,7 +416,7 @@ users.users = {
     home="/home/rvolosatovs";
     description="Roman Volosatovs";
     createHome=true;
-    extraGroups= [ "wheel" "input" "audio" "video" "networkmanager" "docker" ];
+    extraGroups= [ "wheel" "input" "audio" "video" "networkmanager" "docker" "dialout" "tty" "uucp" "disk" "adm" ];
     shell = pkgs.zsh;
   };
 
