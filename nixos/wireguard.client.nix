701136d4a08262d7269e4cae4b606b12b19012d8
Roman Volosatovs <rvolosatovs@riseup.net>
2018-10-15 10:09:37 +0200

    wireguard: Extract shared to nixos

---
+++ nixos/wireguard.client.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+{
+  networking.wireguard.interfaces.wg0.allowedIPsAsRoutes = false;
+
+  networking.wireguard.interfaces.wg0.postSetup = ''
+    ${pkgs.iproute}/bin/ip route add $(${pkgs.iproute}/bin/ip route get "$(${pkgs.wireguard}/bin/wg show wg0 endpoints | sed -n 's/.*\t\(.*\):.*/\1/p')" | sed '/ via [0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/{s/^\(.* via [0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\).*/\1/}' | head -n 1) 2>/dev/null || true
+    ${pkgs.iproute}/bin/ip route add default via 10.0.0.1 dev wg0
+  '';
+
+  powerManagement.resumeCommands = ''
+    ${pkgs.systemd}/bin/systemctl restart wireguard-wg0
+  '';
+
+  systemd.services.wireguard-wg0.after = [ "NetworkManager-wait-online.service" ];
+  systemd.services.wireguard-wg0.wants = [ "NetworkManager-wait-online.service" ];
+}
---
+++ nixos/wireguard.server.nix
@@ -0,0 +1,12 @@
+{ config, ... }:
+{
+  boot.kernel.sysctl."net.ipv4.ip_forward" = 1;
+  boot.kernel.sysctl."net.ipv4.conf.all.proxy_arp" = 1;
+
+  networking.firewall.allowedUDPPorts = [
+    config.resources.wireguard.port
+  ];
+
+  networking.wireguard.interfaces.wg0.listenPort = config.resources.wireguard.port;
+  networking.wireguard.interfaces.wg0.ips = [ "10.0.0.1/24" ];
+}
