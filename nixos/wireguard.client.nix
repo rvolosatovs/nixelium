c94462c36cc19dba8e484f08e747480ada1fb1ba
Roman Volosatovs <rvolosatovs@riseup.net>
2018-11-18 21:36:28 +0100

    wireguard: Remove NetworkManager-wait-online dep

--- nixos/wireguard.client.nix
+++ nixos/wireguard.client.nix
@@ -10,7 +10,4 @@
   powerManagement.resumeCommands = ''
     ${pkgs.systemd}/bin/systemctl restart wireguard-wg0
   '';
-
-  systemd.services.wireguard-wg0.after = [ "NetworkManager-wait-online.service" ];
-  systemd.services.wireguard-wg0.wants = [ "NetworkManager-wait-online.service" ];
 }
