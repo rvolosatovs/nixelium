70f668c5b93fbfcc259dacaa9db2072615acbd76
Roman Volosatovs <rvolosatovs@riseup.net>
2019-04-18 23:58:29 +0200

    home: Adapt to upstream changes

--- home/pass.nix
+++ home/pass.nix
@@ -1,5 +1,5 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [ gopass ];
-  home.sessionVariables.PASSWORD_STORE_DIR = ./../vendor/pass; # TODO: Make this a path that exists and is synced in all environments
+  home.sessionVariables.PASSWORD_STORE_DIR = toString ./../vendor/pass; # TODO: Make this a path that exists and is synced in all environments
 }
