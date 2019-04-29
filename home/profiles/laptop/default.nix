0dac18d479715d5a3e9f6d7f4623a1b49bcf6e6a
Roman Volosatovs <rvolosatovs@riseup.net>
2019-04-29 21:28:58 +0200

    home/profiles/laptop: Remove go packages

--- home/profiles/laptop/default.nix
+++ home/profiles/laptop/default.nix
@@ -49,23 +49,6 @@
     programs.go.enable = true;
     programs.go.goPath = "";
     programs.go.goBin = ".local/bin.go";
-    programs.go.packages = {
-      "github.com/BurntSushi/toml"  = builtins.fetchGit "https://github.com/BurntSushi/toml";
-      "github.com/BurntSushi/xgb"   = builtins.fetchGit "https://github.com/BurntSushi/xgb";
-      "github.com/golang/dep"       = builtins.fetchGit "https://github.com/golang/dep";
-      "github.com/google/go-github" = builtins.fetchGit "https://github.com/google/go-github";
-      "github.com/mohae/deepcopy"   = builtins.fetchGit "https://github.com/mohae/deepcopy";
-      "github.com/oklog/ulid"       = builtins.fetchGit "https://github.com/oklog/ulid";
-      "github.com/pkg/errors"       = builtins.fetchGit "https://github.com/pkg/errors";
-      "github.com/y0ssar1an/q"      = builtins.fetchGit "https://github.com/y0ssar1an/q";
-      "golang.org/x/crypto"         = builtins.fetchGit "https://go.googlesource.com/crypto";
-      "golang.org/x/exp"            = builtins.fetchGit "https://go.googlesource.com/exp";
-      "golang.org/x/oauth2"         = builtins.fetchGit "https://go.googlesource.com/oauth2";
-      "golang.org/x/sys"            = builtins.fetchGit "https://go.googlesource.com/sys";
-      "golang.org/x/text"           = builtins.fetchGit "https://go.googlesource.com/text";
-      "golang.org/x/time"           = builtins.fetchGit "https://go.googlesource.com/time";
-      "google.golang.org/grpc"      = builtins.fetchGit "https://github.com/grpc/grpc-go";
-    };
 
     programs.ssh.extraConfig = ''
       Include config.d/ssh_gateway
