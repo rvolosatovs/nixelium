c7f962738534fdc25a61883d6f66198479dd8701
Roman Volosatovs <roman@thethingsnetwork.org>
2019-05-09 00:08:41 +0200

    darwin: Add NSGlobalDomain customizations

--- darwin/graphical.nix
+++ darwin/graphical.nix
@@ -26,6 +26,26 @@
   system.defaults.finder.FXEnableExtensionChangeWarning = false;
   system.defaults.finder.QuitMenuItem = true;
 
+  system.defaults.NSGlobalDomain."com.apple.mouse.tapBehavior" = 1;
+  system.defaults.NSGlobalDomain."com.apple.swipescrolldirection" = false;
+  system.defaults.NSGlobalDomain."com.apple.trackpad.enableSecondaryClick" = true;
+  system.defaults.NSGlobalDomain."com.apple.trackpad.scaling" = "2.0";
+  system.defaults.NSGlobalDomain."com.apple.trackpad.trackpadCornerClickBehavior" = 1;
+  system.defaults.NSGlobalDomain.AppleKeyboardUIMode = 3;
+  system.defaults.NSGlobalDomain.AppleMeasurementUnits = "Centimeters";
+  system.defaults.NSGlobalDomain.AppleMetricUnits = 1;
+  system.defaults.NSGlobalDomain.ApplePressAndHoldEnabled = true;
+  system.defaults.NSGlobalDomain.AppleShowAllExtensions = true;
+  system.defaults.NSGlobalDomain.AppleShowScrollBars = "Automatic";
+  system.defaults.NSGlobalDomain.AppleTemperatureUnit = "Celsius";
+  system.defaults.NSGlobalDomain.NSAutomaticCapitalizationEnabled = false;
+  system.defaults.NSGlobalDomain.NSAutomaticDashSubstitutionEnabled = true;
+  system.defaults.NSGlobalDomain.NSAutomaticPeriodSubstitutionEnabled = true;
+  system.defaults.NSGlobalDomain.NSAutomaticQuoteSubstitutionEnabled = true;
+  system.defaults.NSGlobalDomain.NSAutomaticSpellingCorrectionEnabled = false;
+  system.defaults.NSGlobalDomain.NSDisableAutomaticTermination = true;
+  system.defaults.NSGlobalDomain.NSDocumentSaveNewDocumentsToCloud = false;
+
   system.defaults.trackpad.Clicking = true;
   system.defaults.trackpad.TrackpadRightClick = true;
   system.defaults.trackpad.TrackpadThreeFingerDrag = true;
