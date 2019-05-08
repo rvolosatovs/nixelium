9e909504cc74b82021c70a07512dc46e4ae6ee3b
Roman Volosatovs <roman@thethingsnetwork.org>
2019-05-08 22:17:40 +0200

    darwin: Add minimal skhd configuration

--- darwin/skhd.nix
+++ darwin/skhd.nix
@@ -6,13 +6,8 @@
 
   services.skhd.enable = true;
   services.skhd.skhdConfig = with config.resources.programs; ''
-    super + Return
-        ${terminal.executable.path}
-
-    super + shift + o
-        ${browser.executable.path}
-
-    super + shift + e
-        ${terminal.executable.path} -e "${shell.executable.path} -i -c ${editor.executable.path}"
+    cmd - return    : ${terminal.executable.path}&
+    cmd + shift - o : ${browser.executable.path}
+    cmd + shift - e : ${terminal.executable.path} -e "${shell.executable.path} -i -c ${editor.executable.path}"
   '';
 }
