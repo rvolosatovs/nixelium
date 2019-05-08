84c98e175e41ccf12d8f2668b2cd3dc886be21d4
Roman Volosatovs <roman@thethingsnetwork.org>
2019-05-08 22:16:39 +0200

    resources: Set work email on krypton

--- resources/hosts/krypton/default.nix
+++ resources/hosts/krypton/default.nix
@@ -5,6 +5,8 @@
 
   config.resources = {
     base16.theme = "tomorrow-night";
+    email = "roman@thethingsnetwork.org";
+    gpg.publicKey.fingerprint = "76A19920EC6421DF1D2B36C9E2F62C4B84AB2D0B";
     graphics.enable = true;
   };
 }
