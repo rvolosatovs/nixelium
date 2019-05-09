e04d3e601013c8caa5b46c5d6ceebb5de7689c00
Roman Volosatovs <rvolosatovs@riseup.net>
2019-05-09 22:50:25 +0200

    darwin: Reorganize skhd

--- darwin/skhd.nix
+++ darwin/skhd.nix
@@ -6,8 +6,8 @@
 
   services.skhd.enable = true;
   services.skhd.skhdConfig = with config.resources.programs; ''
-    cmd - return    : ${terminal.executable.path}&
-    cmd + shift - o : ${browser.executable.path}
     cmd + shift - e : ${terminal.executable.path} -e "${shell.executable.path} -i -c ${editor.executable.path}"
+    cmd + shift - o : ${browser.executable.path}
+    cmd - return    : ${terminal.executable.path}&
   '';
 }
