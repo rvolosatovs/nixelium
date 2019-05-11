b83a411b3f70515f9cb1560a27dd35db4e895273
Roman Volosatovs <roman@thethingsnetwork.org>
2019-05-11 16:20:11 +0200

    home: Rerrange statements, fix Darwin env

--- home/common.nix
+++ home/common.nix
@@ -37,7 +37,6 @@
         rclone
         ripgrep
         shellcheck
-        sudo
         tree
         universal-ctags
         unzip
@@ -54,6 +53,7 @@
 
       home.sessionVariables.__GL_SHADER_DISK_CACHE_PATH ="${config.xdg.cacheHome}/nv";
       home.sessionVariables.BROWSER = config.resources.programs.browser.executable.path;
+      home.sessionVariables.CLICOLOR = "1";
       home.sessionVariables.CUDA_CACHE_PATH = "${config.xdg.cacheHome}/nv";
       home.sessionVariables.EDITOR = config.resources.programs.editor.executable.path;
       home.sessionVariables.ELINKS_CONFDIR = "${config.xdg.configHome}/elinks";
@@ -88,6 +88,8 @@
         Control-k: menu-complete-backward
       '';
       home.sessionVariables.LESSHISTFILE = "${config.xdg.cacheHome}/less/history";
+      home.sessionVariables.LSCOLORS = "ExGxFxdaCxDaDahbadacec";
+      home.sessionVariables.LS_COLORS = "rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:mi=00:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arc=01;31:*.arj=01;31:*.taz=01;31:*.lha=01;31:*.lz4=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.tzo=01;31:*.t7z=01;31:*.zip=01;31:*.z=01;31:*.dz=01;31:*.gz=01;31:*.lrz=01;31:*.lz=01;31:*.lzo=01;31:*.xz=01;31:*.zst=01;31:*.tzst=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.war=01;31:*.ear=01;31:*.sar=01;31:*.rar=01;31:*.alz=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.cab=01;31:*.wim=01;31:*.swm=01;31:*.dwm=01;31:*.esd=01;31:*.jpg=01;35:*.jpeg=01;35:*.mjpg=01;35:*.mjpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.webm=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=00;36:*.au=00;36:*.flac=00;36:*.m4a=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:*.oga=00;36:*.opus=00;36:*.spx=00;36:*.xspf=00;36:";
       home.sessionVariables.MAILER = config.resources.programs.mailer.executable.path;
       home.sessionVariables.MANWIDTH = "80";
       home.sessionVariables.PAGER = config.resources.programs.pager.executable.path;
@@ -209,23 +211,11 @@
     })
 
     (mkIf pkgs.stdenv.isDarwin {
-      home.file.".Brewfile".text = ''
-        brew "choose-gui"
-        cask "brave-browser"
-        cask "keybase"
-        cask "libreoffice"
-        cask "slack"
-        cask "thunderbird"
-        cask "whatsapp"
-        cask "xquartz"
-      '';
-
       home.packages = with pkgs; [
-        coreutils
+        coreutils-prefixed
       ];
 
-      home.sessionVariables.CLICOLOR = "1";
-      home.sessionVariables.LSCOLORS = "ExGxFxdaCxDaDahbadacec";
+      home.file.".terminfo".source = "/etc/profiles/per-user/${config.resources.username}/share/terminfo";
 
       programs.zsh.shellAliases.o = "open";
     })
@@ -233,16 +223,16 @@
     (mkIf pkgs.stdenv.isLinux {
       home.packages = with pkgs; [
         acpi
+        (lib.lowPrio coreutils)
         dex
         espeak
         lm_sensors
         pciutils
         psmisc
         rfkill
+        sudo
         usbutils
       ];
-
-      home.sessionVariables.LS_COLORS = "rs=0:di=01;34:ln=01;36:mh=00:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:mi=00:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arc=01;31:*.arj=01;31:*.taz=01;31:*.lha=01;31:*.lz4=01;31:*.lzh=01;31:*.lzma=01;31:*.tlz=01;31:*.txz=01;31:*.tzo=01;31:*.t7z=01;31:*.zip=01;31:*.z=01;31:*.dz=01;31:*.gz=01;31:*.lrz=01;31:*.lz=01;31:*.lzo=01;31:*.xz=01;31:*.zst=01;31:*.tzst=01;31:*.bz2=01;31:*.bz=01;31:*.tbz=01;31:*.tbz2=01;31:*.tz=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.war=01;31:*.ear=01;31:*.sar=01;31:*.rar=01;31:*.alz=01;31:*.ace=01;31:*.zoo=01;31:*.cpio=01;31:*.7z=01;31:*.rz=01;31:*.cab=01;31:*.wim=01;31:*.swm=01;31:*.dwm=01;31:*.esd=01;31:*.jpg=01;35:*.jpeg=01;35:*.mjpg=01;35:*.mjpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.svg=01;35:*.svgz=01;35:*.mng=01;35:*.pcx=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.m2v=01;35:*.mkv=01;35:*.webm=01;35:*.ogm=01;35:*.mp4=01;35:*.m4v=01;35:*.mp4v=01;35:*.vob=01;35:*.qt=01;35:*.nuv=01;35:*.wmv=01;35:*.asf=01;35:*.rm=01;35:*.rmvb=01;35:*.flc=01;35:*.avi=01;35:*.fli=01;35:*.flv=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.yuv=01;35:*.cgm=01;35:*.emf=01;35:*.ogv=01;35:*.ogx=01;35:*.aac=00;36:*.au=00;36:*.flac=00;36:*.m4a=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:*.oga=00;36:*.opus=00;36:*.spx=00;36:*.xspf=00;36:";
 
       programs.zsh.shellAliases.o = "${pkgs.xdg_utils}/bin/xdg-open";
       programs.zsh.shellAliases.sy="${pkgs.systemd}/bin/systemctl";
--- home/hosts/krypton/default.nix
+++ home/hosts/krypton/default.nix
@@ -1,3 +1,17 @@
 {
+  home.file.".Brewfile".text = ''
+    brew "choose-gui"
+    brew "docker"
+    brew "docker-machine"
+    brew "koekeishiya/formulae/chunkwm"
+    cask "brave-browser"
+    cask "keybase"
+    cask "libreoffice"
+    cask "slack"
+    cask "thunderbird"
+    cask "whatsapp"
+    cask "xquartz"
+  '';
+
   home.sessionVariables.PASSWORD_STORE_DIR = toString ./../../../vendor/pass-ttn-shared;
 }
