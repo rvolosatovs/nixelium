c46bef24833af064a03fef2e41f1da76c3bfc963
Roman Volosatovs <rvolosatovs@riseup.net>
2019-05-13 18:48:42 +0200

    nixos: Remove redundant wireguard-wg0 deps

--- nixos/wireguard.client.nix
+++ nixos/wireguard.client.nix
@@ -6,7 +6,4 @@
     ${pkgs.iproute}/bin/ip route add $(${pkgs.iproute}/bin/ip route get "$(${pkgs.wireguard}/bin/wg show wg0 endpoints | sed -n 's/.*\t\(.*\):.*/\1/p')" | sed '/ via [0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}/{s/^\(.* via [0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\).*/\1/}' | head -n 1) 2>/dev/null || true
     ${pkgs.iproute}/bin/ip route add default via 10.0.0.1 dev wg0
   '';
-
-  systemd.services.wireguard-wg0.after = [ "network-online.target" ];
-  systemd.services.wireguard-wg0.wants = [ "network-online.target" ];
 }
