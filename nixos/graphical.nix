6781da36117baab2a0609f8b291020735eddb2e3
Roman Volosatovs <rvolosatovs@riseup.net>
2019-05-22 11:46:36 +0200

    nixos/graphical: Use keyboard settings from home-manager

--- nixos/graphical.nix
+++ nixos/graphical.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   environment.sessionVariables = {
@@ -53,10 +53,14 @@
   services.xserver.displayManager.lightdm.autoLogin.enable = true;
   services.xserver.displayManager.lightdm.autoLogin.user = config.resources.username;
   services.xserver.displayManager.lightdm.enable = true;
-  services.xserver.displayManager.lightdm.greeters.gtk.theme.package = pkgs.zuki-themes;
   services.xserver.displayManager.lightdm.greeters.gtk.theme.name = "Zukitre";
+  services.xserver.displayManager.lightdm.greeters.gtk.theme.package = pkgs.zuki-themes;
   services.xserver.enable = true;
   services.xserver.exportConfiguration = true;
   services.xserver.windowManager.bspwm.enable = true;
   services.xserver.windowManager.default = "bspwm";
+
+  services.xserver.layout = config.home-manager.users.${config.resources.username}.home.keyboard.layout;
+  services.xserver.xkbOptions = lib.concatStringsSep ", " config.home-manager.users.${config.resources.username}.home.keyboard.options;
+  services.xserver.xkbVariant = config.home-manager.users.${config.resources.username}.home.keyboard.variant;
 }
