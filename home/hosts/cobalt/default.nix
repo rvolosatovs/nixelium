bfc5231ca179804332171cc37bff162f08d31f8c
Roman Volosatovs <rvolosatovs@riseup.net>
2020-02-02 00:37:18 +0100

    home/cobalt: Set SSH aliases

--- home/hosts/cobalt/default.nix
+++ home/hosts/cobalt/default.nix
@@ -1,8 +1,13 @@
-{ pkgs, ... }:
 {
   imports = [
     ../../wine.nix
   ];
 
   home.sessionVariables.PASSWORD_STORE_DIR = toString ./../../../vendor/pass;
+
+  programs.ssh.matchBlocks."conduit".hostname = "conduit.lan";
+  programs.ssh.matchBlocks."kona-micro".hostname = "kona-micro.lan";
+  programs.ssh.matchBlocks."neon".hostname = "neon.lan";
+  programs.ssh.matchBlocks."oxygen".hostname = "oxygen.vpn";
+  programs.ssh.matchBlocks."zinc".hostname = "zinc.lan";
 }
