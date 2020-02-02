5fa45b18414bc7a972d2d88aa4ef7daffdfc6ed8
Roman Volosatovs <rvolosatovs@riseup.net>
2020-02-02 01:34:47 +0100

    home/{neon,cobalt}: Configure gateway SSH

--- home/hosts/cobalt/default.nix
+++ home/hosts/cobalt/default.nix
@@ -6,7 +6,10 @@
   home.sessionVariables.PASSWORD_STORE_DIR = toString ./../../../vendor/pass;
 
   programs.ssh.matchBlocks."conduit".hostname = "conduit.lan";
+  programs.ssh.matchBlocks."conduit".user = "admin";
   programs.ssh.matchBlocks."kona-micro".hostname = "kona-micro.lan";
+  programs.ssh.matchBlocks."kona-micro".user = "root";
+
   programs.ssh.matchBlocks."neon".hostname = "neon.lan";
   programs.ssh.matchBlocks."neon-luks".hostname = "neon.lan";
   programs.ssh.matchBlocks."oxygen".hostname = "oxygen.external";
--- home/hosts/neon/default.nix
+++ home/hosts/neon/default.nix
@@ -1,9 +1,12 @@
 {
   home.sessionVariables.PASSWORD_STORE_DIR = toString ./../../../vendor/pass;
 
-  programs.ssh.matchBlocks."cobalt".hostname = "cobalt.lan";
   programs.ssh.matchBlocks."conduit".hostname = "conduit.lan";
+  programs.ssh.matchBlocks."conduit".user = "admin";
   programs.ssh.matchBlocks."kona-micro".hostname = "kona-micro.lan";
+  programs.ssh.matchBlocks."kona-micro".user = "root";
+
+  programs.ssh.matchBlocks."cobalt".hostname = "cobalt.lan";
   programs.ssh.matchBlocks."oxygen".hostname = "oxygen.external";
   programs.ssh.matchBlocks."oxygen-luks".hostname = "oxygen.external";
   programs.ssh.matchBlocks."zinc".hostname = "zinc.lan";
