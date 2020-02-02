df61c416df89be5e567940a0b27b45bd7632f953
Roman Volosatovs <rvolosatovs@riseup.net>
2020-02-02 01:26:40 +0100

    home/{neon,cobalt}: Use external oxygen IP for SSH

--- home/hosts/cobalt/default.nix
+++ home/hosts/cobalt/default.nix
@@ -9,7 +9,7 @@
   programs.ssh.matchBlocks."kona-micro".hostname = "kona-micro.lan";
   programs.ssh.matchBlocks."neon".hostname = "neon.lan";
   programs.ssh.matchBlocks."neon-luks".hostname = "neon.lan";
-  programs.ssh.matchBlocks."oxygen".hostname = "oxygen.vpn";
+  programs.ssh.matchBlocks."oxygen".hostname = "oxygen.external";
   programs.ssh.matchBlocks."oxygen-luks".hostname = "oxygen.external";
   programs.ssh.matchBlocks."zinc".hostname = "zinc.lan";
 }
--- home/hosts/neon/default.nix
+++ home/hosts/neon/default.nix
@@ -4,7 +4,7 @@
   programs.ssh.matchBlocks."cobalt".hostname = "cobalt.lan";
   programs.ssh.matchBlocks."conduit".hostname = "conduit.lan";
   programs.ssh.matchBlocks."kona-micro".hostname = "kona-micro.lan";
-  programs.ssh.matchBlocks."oxygen".hostname = "oxygen.vpn";
+  programs.ssh.matchBlocks."oxygen".hostname = "oxygen.external";
   programs.ssh.matchBlocks."oxygen-luks".hostname = "oxygen.external";
   programs.ssh.matchBlocks."zinc".hostname = "zinc.lan";
 }
