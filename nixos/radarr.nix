42a2f101cc7f0648dfe23a85c750be5de20c9227
Roman Volosatovs <rvolosatovs@riseup.net>
2020-02-12 02:46:39 +0100

    nixos/{rad,son}arr: Unrus movies/episodes

--- nixos/radarr.nix
+++ nixos/radarr.nix
@@ -1,5 +1,13 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
+  environment.systemPackages = with pkgs; [
+    (pkgs.writeShellScriptBin "radarr-unrus" ''
+      if [ ''${radarr_eventtype} == "Download" ]; then
+        ${mkUnrusScript "\${radarr_movie_path}"}
+      fi
+    '')
+  ];
+
   services.radarr.enable = true;
 
   users.users.${config.resources.username}.extraGroups = [ "radarr" ];
--- nixos/sonarr.nix
+++ nixos/sonarr.nix
@@ -1,5 +1,13 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
+  environment.systemPackages = with pkgs; [
+    (pkgs.writeShellScriptBin "sonarr-unrus" ''
+      if [ ''${sonarr_eventtype} == "Download" ]; then
+        ${mkUnrusScript "\${sonarr_series_path}"}
+      fi
+    '')
+  ];
+
   services.sonarr.enable = true;
 
   users.users.${config.resources.username}.extraGroups = [ "sonarr" ];
