e1a442098e1c087d81acd998776d96db7879fc74
Roman Volosatovs <rvolosatovs@riseup.net>
2020-03-03 23:51:00 +0100

    nixops/networks/private: Refactor, disable oxygen

--- nixops/networks/private/default.nix
+++ nixops/networks/private/default.nix
@@ -1,75 +1,9 @@
 let
-  wg.interfaceName = "wg0";
-  wg.privateKeyName = "wireguard-${wg.interfaceName}-private";
+  # TODO: Configure these in WireGuard module, once supported.
+  wg.home.hosts.zinc.ip = "10.0.0.30";
+  wg.home.hosts.zinc.publicKey = "QLMUw+yvwXuuEsN06zB+Mj9n/VqD+k4VKa5o2GZrLAk=";
 
-  wg.hosts.cobalt.ip = "10.0.0.11";
-  wg.hosts.cobalt.publicKey = "6L6uG3nflK0GJt1468gV38jWX1BkVIj22XuqXtE99gk=";
-  wg.hosts.cobalt.privateKey = builtins.readFile ./../../../vendor/secrets/nixos/hosts/cobalt/wg.private;
-
-  wg.hosts.neon.ip = "10.0.0.10";
-  wg.hosts.neon.publicKey = "weyoU0QBHrnjl+2dhibGm5um9+f2sZrg9x8SFd2AVhc=";
-  wg.hosts.neon.privateKey = builtins.readFile ./../../../vendor/secrets/nixos/hosts/neon/wg.private;
-
-  wg.hosts.oxygen.ip = "10.0.0.1";
-  wg.hosts.oxygen.publicKey = "xjzZIo0SKBNtwP/puZU4cMDdhOsdeMvC/qEKh6RAuAo=";
-  wg.hosts.oxygen.privateKey = builtins.readFile ./../../../vendor/secrets/nixos/hosts/oxygen/wg.private;
-
-  wg.hosts.zinc.ip = "10.0.0.30";
-  wg.hosts.zinc.publicKey = "QLMUw+yvwXuuEsN06zB+Mj9n/VqD+k4VKa5o2GZrLAk=";
-
-  wg.serverName = "oxygen";
-  wg.server = wg.hosts.${wg.serverName};
-
-  wg.hostNames = builtins.attrNames wg.hosts;
-
-  mkVPNNode = lib: {
-    networking.hosts = with lib; foldr (name: hosts: mkMerge [
-      hosts
-      {
-        "${wg.hosts.${name}.ip}" = [ "${name}.vpn" ];
-      }
-    ]) {} wg.hostNames;
-  };
-
-  mkVPNBypassRule = ip: ''
-    [RoutingPolicyRule]
-    To=${ip}
-    Table=2468
-  '';
-
-  mkVPNClient = lib: name: nodes: with lib; mkMerge [
-    (mkVPNNode lib)
-    {
-      systemd.network.netdevs."30-${wg.interfaceName}" = {
-        netdevConfig.Kind = "wireguard";
-        netdevConfig.Name = wg.interfaceName;
-        extraConfig = ''
-          [WireGuard]
-          PrivateKey=${wg.hosts.${name}.privateKey}
-
-          [WireGuardPeer]
-          PublicKey=${wg.server.publicKey}
-          AllowedIPs=0.0.0.0/0, ::/0
-          Endpoint=${nodes.${wg.serverName}.config.deployment.targetHost}:${toString nodes.${wg.serverName}.config.networking.wireguard.interfaces.${wg.interfaceName}.listenPort}
-          PersistentKeepalive=25
-        '';
-      };
-
-      systemd.network.networks."30-${wg.interfaceName}" = {
-        matchConfig.Name = wg.interfaceName;
-        networkConfig.Address = "${wg.hosts.${name}.ip}/32";
-        routes = singleton {
-          routeConfig.Destination = "0.0.0.0/0";
-          routeConfig.Gateway = wg.server.ip;
-          routeConfig.GatewayOnLink = "true";
-        };
-        extraConfig = concatMapStringsSep "\n" mkVPNBypassRule [
-          nodes.${wg.serverName}.config.deployment.targetHost
-        ];
-      };
-    }
-  ];
-
+  # TODO: Refactor into a NixOS module.
   mkNginxTLSProxy = config: name: addr: {
     services.nginx.enable = true;
     services.nginx.virtualHosts."${name}" = {
@@ -80,80 +14,109 @@ let
     };
   };
 
+  mkBorgRepoPath = config: name: "${config.services.borgbackup.repos.${name}.user}@${config.network.wireguard.ip}:${config.services.borgbackup.repos.${name}.path}";
+
   isLANip = lib: ip: lib.strings.hasPrefix "192.168." ip;
 in
   {
     network.description = "Private network of rvolosatovs";
     network.enableRollback = true;
 
-    defaults.networking.firewall.trustedInterfaces = [ wg.interfaceName ];
+    defaults.network.wireguard.enable = true;
+    defaults.network.wireguard.interfaceName = "wg-private";
+    defaults.network.wireguard.server.name = "neon";
+    defaults.network.wireguard.subnet = "10.0.0.0/24";
 
-    cobalt = { config, lib, name, nodes, ... }: {
+    cobalt = { config, lib, nodes, ... }: {
       imports = [
         ./../../../nixos/hosts/cobalt
         ./../../../vendor/secrets/nixops/hosts/cobalt
         ./../../profiles/laptop
       ];
 
-      config = lib.mkMerge [
-        (mkVPNClient lib name nodes)
-        {
-          deployment.hasFastConnection = true;
-        }
-      ];
+      config = {
+        deployment.hasFastConnection = true;
+
+        network.wireguard.ip = "10.0.0.11";
+        network.wireguard.publicKey = "6L6uG3nflK0GJt1468gV38jWX1BkVIj22XuqXtE99gk=";
+        network.wireguard.privateKey = builtins.readFile ./../../../vendor/secrets/nixops/hosts/cobalt/wg.home.private;
+
+        networking.privateIPv4 = "192.168.188.11";
+
+        services.btrfs.snapshotBackup.enable = true;
+        services.btrfs.snapshotBackup.subvolumes."/.snapshots" = {
+          repo = mkBorgRepoPath nodes.neon.config "root";
+          passphrase = builtins.readFile ./../../../vendor/secrets/nixos/hosts/neon/borg.root.repokey;
+          sshKey = builtins.readFile ./../../../vendor/secrets/nixops/hosts/cobalt/borg.root.id_ed25519;
+        };
+        services.btrfs.snapshotBackup.subvolumes."/home/.snapshots" = {
+          repo = mkBorgRepoPath nodes.neon.config "home";
+          passphrase = builtins.readFile ./../../../vendor/secrets/nixos/hosts/neon/borg.home.repokey;
+          sshKey = builtins.readFile ./../../../vendor/secrets/nixops/hosts/cobalt/borg.home.id_ed25519;
+        };
+      };
     };
 
-    neon = { config, lib, name, nodes, ... }: {
+    neon = { config, lib, nodes, ... }: {
       imports = [
         ./../../../nixos/hosts/neon
         ./../../../vendor/secrets/nixops/hosts/neon
         ./../../deluge.nix
-        ./../../profiles/laptop
-      ];
-
-      config = lib.mkMerge [
-        (mkVPNClient lib name nodes)
-        {
-          deployment.hasFastConnection = isLANip lib config.deployment.targetHost;
-        }
-      ];
-    };
-
-    oxygen = { config, lib, name, ... }: {
-      imports = [
-        ./../../../nixos/hosts/oxygen
-        ./../../../nixos/wireguard.server.nix
-        ./../../../vendor/secrets/nixops/hosts/oxygen
         ./../../meet.nix
         ./../../miniflux.nix
         ./../../profiles/server
       ];
-      
-      config = with lib; mkMerge [
-        (mkVPNNode lib)
-        (mkNginxTLSProxy config "deluge" "http://${wg.hosts.neon.ip}:8112")
-        (mkNginxTLSProxy config "jackett" "http://${wg.hosts.neon.ip}:9117")
-        (mkNginxTLSProxy config "lidarr" "http://${wg.hosts.neon.ip}:8686")
-        (mkNginxTLSProxy config "radarr" "http://${wg.hosts.neon.ip}:7878")
-        (mkNginxTLSProxy config "sonarr" "http://${wg.hosts.neon.ip}:8989")
-        {
-          deployment.hasFastConnection = isLANip lib config.deployment.targetHost;
-      
-          deployment.keys.${wg.privateKeyName}.text = wg.hosts.${name}.privateKey;
-      
-          networking.wireguard.interfaces.${wg.interfaceName} = {
-            peers = foldr (peerName: peers: peers ++ optional (peerName != name) {
-              allowedIPs = [ "${wg.hosts.${peerName}.ip}/32" ];
-              publicKey = wg.hosts.${peerName}.publicKey;
-            }) [] wg.hostNames;
-            privateKeyFile = config.deployment.keys.${wg.privateKeyName}.path;
-          };
-      
-          systemd.services."wireguard-${wg.interfaceName}" = {
-            after = [ "${wg.privateKeyName}-key.service" ];
-            wants = [ "${wg.privateKeyName}-key.service" ];
-          };
-        }
-      ];
+
+      config = {
+        deployment.hasFastConnection = isLANip lib config.deployment.targetHost;
+        deployment.targetHost = config.networking.privateIPv4;
+
+        network.wireguard.ip = "10.0.0.10";
+        network.wireguard.publicKey = "weyoU0QBHrnjl+2dhibGm5um9+f2sZrg9x8SFd2AVhc=";
+        network.wireguard.privateKey= builtins.readFile ./../../../vendor/secrets/nixops/hosts/neon/wg.home.private;
+
+        networking.privateIPv4 = "192.168.188.10";
+        # TODO: Figure out why host is unreachable from outside LAN.
+        networking.publicIPv4 = lib.mkForce config.networking.privateIPv4;
+      };
     };
+
+    # TODO: Rent a VPS and migrate oxygen.
+    #oxygen = { config, lib, name, ... }: {
+    #  imports = [
+    #    ./../../../nixos/hosts/oxygen
+    #    ./../../../nixos/wireguard.server.nix
+    #    ./../../../vendor/secrets/nixops/hosts/oxygen
+    #    ./../../meet.nix
+    #    ./../../miniflux.nix
+    #    ./../../profiles/server
+    #  ];
+    #  
+    #  config = with lib; mkMerge [
+    #    (mkVPNNode lib)
+    #    (mkNginxTLSProxy config "deluge" "http://${wg.hosts.neon.ip}:8112")
+    #    (mkNginxTLSProxy config "jackett" "http://${wg.hosts.neon.ip}:9117")
+    #    (mkNginxTLSProxy config "lidarr" "http://${wg.hosts.neon.ip}:8686")
+    #    (mkNginxTLSProxy config "radarr" "http://${wg.hosts.neon.ip}:7878")
+    #    (mkNginxTLSProxy config "sonarr" "http://${wg.hosts.neon.ip}:8989")
+    #    {
+    #      deployment.hasFastConnection = isLANip lib config.deployment.targetHost;
+    #  
+    #      deployment.keys.${wg.homeKeyName}.text = wg.hosts.${name}.privateKey;
+    #  
+    #      networking.wireguard.interfaces.${wg.home.interfaceName} = {
+    #        peers = foldr (peerName: peers: peers ++ optional (peerName != name) {
+    #          allowedIPs = [ "${wg.hosts.${peerName}.ip}/32" ];
+    #          publicKey = wg.hosts.${peerName}.publicKey;
+    #        }) [] wg.hostNames;
+    #        privateKeyFile = config.deployment.keys.${wg.homeKeyName}.path;
+    #      };
+    #  
+    #      systemd.services."wireguard-${wg.home.interfaceName}" = {
+    #        after = [ "${wg.homeKeyName}-key.service" ];
+    #        wants = [ "${wg.homeKeyName}-key.service" ];
+    #      };
+    #    }
+    #  ];
+    #};
   }
