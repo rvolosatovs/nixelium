9a0f85d5e023a56dbb20432219b4355f66b01173
Roman Volosatovs <rvolosatovs@riseup.net>
2020-09-09 21:21:21 +0200

    *: Remove unused hosts

--- home/hosts/oxygen/default.nix
+++
@@ -1,6 +0,0 @@
-{
-  programs.ssh.matchBlocks."cobalt".hostname = "cobalt.vpn";
-  programs.ssh.matchBlocks."neon".hostname = "neon.vpn";
-  programs.ssh.matchBlocks."neon-luks".hostname = "neon.vpn";
-  programs.ssh.matchBlocks."zinc".hostname = "zinc.vpn";
-}
