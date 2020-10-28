819655b6b10b2d1290bcb497df41ea025e0a2aee
Roman Volosatovs <rvolosatovs@riseup.net>
2020-10-28 19:13:43 +0100

    nixos/x395: Disable usb suspend in TLP

--- nixos/hardware/lenovo/thinkpad/amd/x395/default.nix
+++ nixos/hardware/lenovo/thinkpad/amd/x395/default.nix
@@ -18,11 +18,15 @@ in {
   networking.interfaces."${wiredInterface}".useDHCP = true;
   networking.interfaces."${wirelessInterface}".useDHCP = true;
 
-  # See https://linrunner.de/en/tlp/docs/tlp-faq.html#battery https://wiki.archlinux.org/index.php/TLP#Btrfs
+  # Related:
+  # - https://linrunner.de/en/tlp/docs/tlp-faq.html#battery
+  # - https://wiki.archlinux.org/index.php/TLP#Btrfs
+  # - https://wiki.archlinux.org/index.php/Lenovo_ThinkPad_X395
   services.tlp.settings.CPU_BOOST_ON_AC = 1;
   services.tlp.settings.CPU_BOOST_ON_BAT = 0;
   services.tlp.settings.CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
   services.tlp.settings.ENERGY_PERF_POLICY_ON_BAT = "powersave";
+  services.tlp.settings.RUNTIME_PM_BLACKLIST = "05:00.3";
   services.tlp.settings.SATA_LINKPWR_ON_BAT = "max_performance";
   services.tlp.settings.START_CHARGE_THRESH_BAT0 = 75;
   services.tlp.settings.STOP_CHARGE_THRESH_BAT0 = 80;
