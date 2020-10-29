1e880506dc456108b0840c9fbbf0b35af5a089d0
Roman Volosatovs <rvolosatovs@riseup.net>
2020-10-29 10:43:31 +0100

    nixos/x260: Remove conflicting TLP setting

--- nixos/hardware/lenovo/thinkpad/intel/x260/default.nix
+++ nixos/hardware/lenovo/thinkpad/intel/x260/default.nix
@@ -26,7 +26,6 @@ in
   services.tlp.settings.CPU_BOOST_ON_BAT = 0;
   services.tlp.settings.CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
   services.tlp.settings.ENERGY_PERF_POLICY_ON_BAT = "powersave";
-  services.tlp.settings.SATA_LINKPWR_ON_BAT = "max_performance";
   services.tlp.settings.START_CHARGE_THRESH_BAT0 = 75;
   services.tlp.settings.START_CHARGE_THRESH_BAT1 = 75;
   services.tlp.settings.STOP_CHARGE_THRESH_BAT0 = 80;
