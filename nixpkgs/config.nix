dc62511abb5ca5ce4b75f43845ed5c10a7ac89a5
Roman Volosatovs <rvolosatovs@riseup.net>
2021-08-10 13:46:17 +0200

    nixpkgs: disallow flash in firefox

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -3,6 +3,8 @@
   allowUnfree = true;
   allowUnfreeRedistributable = true;
 
+  config.firefox.enableAdobeFlash = false;
+
   permittedInsecurePackages = [
     "p7zip-16.02"
     "python2.7-certifi-2019.11.28"
