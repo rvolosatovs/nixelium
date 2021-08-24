ff08139d749df4bae21082155e7dbe7f54ee8e68
Roman Volosatovs <rvolosatovs@riseup.net>
2021-08-24 15:09:24 +0200

    home/krypton: update brew packages and pass path

--- home/hosts/krypton/default.nix
+++ home/hosts/krypton/default.nix
@@ -1,20 +1,12 @@
 {
   home.file.".Brewfile".text = ''
-    tap "homebrew/cask"
-    tap "homebrew/services"
-    tap "koekeishiya/formulae"
-
     brew "choose-gui"
-    brew "chunkwm"
-    brew "ykman"
-
-    cask "brave-browser"
-    cask "docker"
-    cask "keybase"
-    cask "libreoffice"
-    cask "thunderbird"
-    cask "xquartz"
+    brew "docker"
+    brew "firefox"
+    brew "keybase"
+    brew "koekeishiya/formulae/yabai"
+    brew "thunderbird"
   '';
 
-  home.sessionVariables.PASSWORD_STORE_DIR = toString ./../../../vendor/pass-ttn-shared;
+  home.sessionVariables.PASSWORD_STORE_DIR = toString ./../../../vendor/pass;
 }
