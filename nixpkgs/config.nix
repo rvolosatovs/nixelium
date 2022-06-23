41cbde161edc90dae22afca9e8858a8f0a5e66ed
Roman Volosatovs <rvolosatovs@riseup.net>
2022-06-23 21:22:35 +0200

    nixpkgs: permit pyjwt-1.7.1
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -6,6 +6,9 @@
   config.firefox.enableAdobeFlash = false;
   config.firefox.drmSupport = true;
 
+  permittedInsecurePackages = [
+    "python2.7-pyjwt-1.7.1"
+  ];
 
   wine.build = "wineWow";
   wine.release = "staging";
