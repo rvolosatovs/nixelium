9a132a8a57fb26ddc45c03b14d4916c091f7c7d5
Roman Volosatovs <rvolosatovs@riseup.net>
2022-06-23 13:15:14 +0200

    nixpkgs: disallow insecure packages
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- nixpkgs/config.nix
+++ nixpkgs/config.nix
@@ -6,11 +6,6 @@
   config.firefox.enableAdobeFlash = false;
   config.firefox.drmSupport = true;
 
-  permittedInsecurePackages = [
-    "p7zip-16.02"
-    "python2.7-certifi-2019.11.28"
-  ];
-
   pulseaudio = true;
 
   wine.build = "wineWow";
