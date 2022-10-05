169ede1d71001c430d015f5cd7ebcb42cf761f2e
Roman Volosatovs <roman@profian.com>
2022-10-05 14:09:16 +0200

    refactor: sort imports lexicographically
    Signed-off-by: Roman Volosatovs <roman@profian.com>

--- nixosModules/default.nix
+++ nixosModules/default.nix
@@ -1,12 +1,12 @@
 inputs @ {...}: {
   benefice = import ./benefice.nix inputs;
-  common = import ./common.nix inputs;
   cloudflared = import ./cloudflared.nix inputs;
+  common = import ./common.nix inputs;
   drawbridge = import ./drawbridge.nix inputs;
+  monitoring = import ./monitoring.nix inputs;
   providers = import ./providers.nix inputs;
   service = import ./service.nix inputs;
   shells = import ./shells.nix inputs;
   steward = import ./steward.nix inputs;
-  monitoring = import ./monitoring.nix inputs;
   users = import ./users.nix inputs;
 }
