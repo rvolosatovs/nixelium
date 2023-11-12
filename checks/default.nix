de724a75619ff5aaf271015db440f7e4a473bbaf
Roman Volosatovs <rvolosatovs@riseup.net>
2023-11-13 00:24:07 +0100

    chore: setup CI
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- checks/default.nix
+++ checks/default.nix
@@ -1,8 +1,16 @@
-{self, ...} @ inputs: pkgs: {
-  lib = import ./lib.nix inputs pkgs;
+{self, ...} @ inputs: pkgs:
+with pkgs.lib;
+  {
+    lib = import ./lib.nix inputs pkgs;
 
-  cobalt = self.nixosConfigurations.cobalt.config.system.build.toplevel;
-  osmium = self.nixosConfigurations.osmium.config.system.build.toplevel;
+    neovim = self.packages.${pkgs.hostPlatform.system}.neovim;
+  }
+  // optionalAttrs pkgs.stdenv.hostPlatform.isLinux {
+    install-iso = self.packages.${pkgs.hostPlatform.system}.install-iso;
 
-  install-iso = self.packages.${pkgs.hostPlatform.system}.install-iso;
-}
+    cobalt = self.nixosConfigurations.cobalt.config.system.build.toplevel;
+    osmium = self.nixosConfigurations.osmium.config.system.build.toplevel;
+  }
+  // optionalAttrs pkgs.stdenv.hostPlatform.isDarwin {
+    iridium = self.darwinConfigurations.iridium.config.system.build.toplevel;
+  }
