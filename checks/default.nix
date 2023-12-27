3bf97174facd3032d471047de13325bbaef3eb86
Roman Volosatovs <rvolosatovs@riseup.net>
2023-12-27 14:39:09 +0100

    refactor: do not build systems in checks
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- checks/default.nix
+++ checks/default.nix
@@ -1,19 +1,5 @@
-{self, ...} @ inputs: pkgs:
-with pkgs.lib;
-  {
-    lib = import ./lib.nix inputs pkgs;
+{self, ...} @ inputs: pkgs: {
+  lib = import ./lib.nix inputs pkgs;
 
-    neovim = self.packages.${pkgs.hostPlatform.system}.neovim;
-  }
-  // optionalAttrs (pkgs.stdenv.hostPlatform.isLinux && pkgs.stdenv.hostPlatform.isx86_64) {
-    install-iso-x86_64-linux = self.packages.${pkgs.hostPlatform.system}.install-iso-x86_64-linux;
-
-    cobalt = self.nixosConfigurations.cobalt.config.system.build.toplevel;
-    osmium = self.nixosConfigurations.osmium.config.system.build.toplevel;
-  }
-  // optionalAttrs (pkgs.stdenv.hostPlatform.isLinux && pkgs.stdenv.hostPlatform.isAarch64) {
-    install-iso-aarch64-linux = self.packages.${pkgs.hostPlatform.system}.install-iso-aarch64-linux;
-  }
-  // optionalAttrs (pkgs.stdenv.hostPlatform.isDarwin && pkgs.stdenv.hostPlatform.isAarch64) {
-    iridium = self.darwinConfigurations.iridium.config.system.build.toplevel;
-  }
+  neovim = self.packages.${pkgs.hostPlatform.system}.neovim;
+}
