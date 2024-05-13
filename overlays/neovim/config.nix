d1dce8fe4d95e4c8fc67f90586053aa348136e00
Roman Volosatovs <rvolosatovs@riseup.net>
2024-05-13 17:03:52 +0200

    Revert "chore(nvim): switch to stable rust-analyzer"
    This reverts commit f30782c16189f98c0d6a6c2ce947290924339f87.

--- overlays/neovim/config.nix
+++ overlays/neovim/config.nix
@@ -3,7 +3,6 @@
   julia,
   pkgsUnstable,
   ripgrep,
-  rust-analyzer,
   stdenv,
   ...
 }: let
@@ -24,7 +23,7 @@
     bin.nil = "${pkgsUnstable.nil}/bin/nil";
     bin.omnisharp = "${pkgsUnstable.omnisharp-roslyn}/bin/omnisharp";
     bin.ripgrep = "${ripgrep}/bin/rg";
-    bin.rust-analyzer = "${rust-analyzer}/bin/rust-analyzer";
+    bin.rust-analyzer = "${pkgsUnstable.rust-analyzer}/bin/rust-analyzer";
     bin.taplo = "${pkgsUnstable.taplo}/bin/taplo";
     src.lua-language-server = "${pkgsUnstable.sumneko-lua-language-server}";
   };
