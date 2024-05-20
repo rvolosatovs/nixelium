6791041bd72233565dcf8e6d73b4817da00c53d0
Roman Volosatovs <rvolosatovs@riseup.net>
2024-05-20 08:46:17 +0200

    build(nix): switch to nixpkgs-unstable
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- flake.nix
+++ flake.nix
@@ -49,7 +49,7 @@
   inputs.nixpkgs-firefox-darwin.url = "github:bandithedoge/nixpkgs-firefox-darwin";
   inputs.nixpkgs-legacy.url = "github:NixOS/nixpkgs/nixos-23.05";
   inputs.nixpkgs-nixos.url = "github:NixOS/nixpkgs/nixos-23.11";
-  inputs.nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.nixpkgs-yabai.url = "github:NixOS/nixpkgs/8d803996c702c3cc244c98d7fe5cb638441f9d93";
   inputs.sops-nix.inputs.nixpkgs-stable.follows = "nixpkgs-nixos";
   inputs.sops-nix.inputs.nixpkgs.follows = "nixpkgs-unstable";
