9de6dedbb958d85092b11d566674f9b9b26eab08
Roman Volosatovs <rvolosatovs@riseup.net>
2026-04-17 16:55:10 +0200

    home(eza): show group
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- homeModules/nixelium.nix
+++ homeModules/nixelium.nix
@@ -495,6 +495,7 @@ in
       programs.eza.enable = true;
       programs.eza.enableBashIntegration = true;
       programs.eza.enableZshIntegration = true;
+      programs.eza.extraOptions = [ "--group" ];
       programs.eza.git = true;
 
       programs.git.enable = true;
