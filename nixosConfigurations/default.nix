2c21b0a00363b7ece432c60344f8c9138841ca32
Roman Volosatovs <rvolosatovs@riseup.net>
2026-04-29 18:04:56 +0200

    feat: add linux vm
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- nixosConfigurations/default.nix
+++ nixosConfigurations/default.nix
@@ -3,6 +3,7 @@ inputs: {
   #install-aarch64 = import ./install-aarch64 inputs;
   #install-x86_64 = import ./install-x86_64 inputs;
 
+  aarch64-linux-vm-nixvm = import ./aarch64-linux-vm-nixvm inputs;
   cobalt = import ./cobalt inputs;
   osmium = import ./osmium inputs;
 }
