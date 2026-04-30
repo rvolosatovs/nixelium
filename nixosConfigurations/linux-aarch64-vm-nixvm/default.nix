6067527aea2832327fcdf01e5959176c20f3733b
Roman Volosatovs <rvolosatovs@riseup.net>
2026-04-30 17:15:01 +0200

    feat(vm): add common AI packages
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- nixosConfigurations/linux-aarch64-vm-nixvm/default.nix
+++ nixosConfigurations/linux-aarch64-vm-nixvm/default.nix
@@ -20,7 +20,13 @@ nixpkgs-nixos.lib.nixosSystem {
           pkgs.codex
           pkgs.rustup
 
+          pkgs.pkgsUnstable.bun
+          pkgs.pkgsUnstable.gemini-cli
+          pkgs.pkgsUnstable.gh
           pkgs.pkgsUnstable.github-copilot-cli
+          pkgs.pkgsUnstable.nodejs
+          pkgs.pkgsUnstable.python3
+          pkgs.pkgsUnstable.uv
         ];
 
         networking.hostName = "linux-aarch64-vm";
