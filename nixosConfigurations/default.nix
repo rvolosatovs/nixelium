2bb36da54b95e99b2651629efd73fab5d05fb295
Roman Volosatovs <rvolosatovs@riseup.net>
2026-05-01 16:09:25 +0200

    chore: drop redundant vm prefix
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- nixosConfigurations/default.nix
+++ nixosConfigurations/default.nix
@@ -3,7 +3,7 @@ inputs: {
   #install-aarch64 = import ./install-aarch64 inputs;
   #install-x86_64 = import ./install-x86_64 inputs;
 
-  linux-aarch64-vm-nixvm = import ./linux-aarch64-vm-nixvm inputs;
+  aarch64-vm-nixvm = import ./aarch64-vm-nixvm inputs;
 
   cobalt = import ./cobalt inputs;
   osmium = import ./osmium inputs;
