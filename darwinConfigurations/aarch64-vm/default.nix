3aa0807935cca324deed54efb7c7f5638dc61092
Roman Volosatovs <rvolosatovs@riseup.net>
2026-05-05 11:51:14 +0200

    feat: add `unrestricted-ai` profile
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

--- darwinConfigurations/aarch64-vm/default.nix
+++ darwinConfigurations/aarch64-vm/default.nix
@@ -8,17 +8,22 @@ with flake-utils.lib.system;
 nix-darwin.lib.darwinSystem {
   system = aarch64-darwin;
   modules = [
-    ({
-      imports = [ self.darwinModules.default ];
+    (
+      { config, ... }:
+      {
+        imports = [ self.darwinModules.default ];
 
-      networking.hostName = "darwin-aarch64-vm";
+        networking.hostName = "darwin-aarch64-vm";
 
-      nixelium.profile.dev.enable = true;
-      nixelium.profile.vm.enable = true;
+        nixelium.profile.dev.enable = true;
+        nixelium.profile.vm.enable = true;
 
-      security.sudo.extraConfig = ''
-        %admin ALL=(ALL) NOPASSWD: ALL
-      '';
-    })
+        home-manager.users.${config.system.primaryUser}.nixelium.profile.unrestricted-ai.enable = true;
+
+        security.sudo.extraConfig = ''
+          %admin ALL=(ALL) NOPASSWD: ALL
+        '';
+      }
+    )
   ];
 }
