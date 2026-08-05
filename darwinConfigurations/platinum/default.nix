9a67bc325b3292625771ff3b4e555a29c11a2035
Roman Volosatovs <rvolosatovs@riseup.net>
2026-08-05 10:56:00 -0400

    mac: add `platinum`
    Signed-off-by: Roman Volosatovs <rvolosatovs@riseup.net>

---
+++ darwinConfigurations/platinum/default.nix
@@ -0,0 +1,32 @@
+{
+  self,
+  flake-utils,
+  nix-darwin,
+  ...
+}:
+with flake-utils.lib.system;
+nix-darwin.lib.darwinSystem {
+  system = aarch64-darwin;
+  modules = [
+    (
+      { ... }:
+      {
+        imports = [ self.darwinModules.default ];
+
+        networking.hostName = "platinum";
+
+        networking.knownNetworkServices = [
+          "Thunderbolt Bridge"
+          "Wi-Fi"
+        ];
+
+        home-manager.sharedModules = [ { home.stateVersion = "26.05"; } ];
+
+        nixelium.profile.dev.enable = true;
+        nixelium.profile.laptop.enable = true;
+
+        system.stateVersion = 7;
+      }
+    )
+  ];
+}
